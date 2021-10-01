321c7b1efe3ccbde9943af2756697d2d44dba15c
rounak777 <rounaksinghrajput777@gmail.com>
2021-10-02 00:30:38 +0530

    Implementation of pagination, sorting, filtering

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,21 +7,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import {ToastrModule} from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
-import { NotFoundComponent } from './errors/not-found/not-found.component';
-import { ErrorInterceptor } from './_interceptors/error.interceptor';
-import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
-import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
@@ -39,9 +37,9 @@ import { DateInputComponent } from './_forms/date-input/date-input.component';
     MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
+    TestErrorsComponent,
     NotFoundComponent,
     ServerErrorComponent,
-    TestErrorsComponent,
     MemberCardComponent,
     MemberEditComponent,
     PhotoEditorComponent,
